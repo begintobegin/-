0d92e36bfd858d818e68667d0c45fd29d2c88a6b
begintobegin <99593957+begintobegin@users.noreply.github.com>
2023-07-16 13:03:51 +0800

    Update event.js

--- js/event.js
+++ js/event.js
@@ -30,8 +30,28 @@ function getParent(elemSelector, parentSelector) {
 
   return null;
 }
+function eventclick(elem){
+let SwiperHeader = document.querySelector(".slider__wrapper-header");
+ let headerMain = document.querySelector(".header");
+ let headerBody = document.querySelector(".header__body");
+ let headerfooter = document.querySelector(".header__footer");
+ if(SwiperHeader.style.display == "none"){
+  headerMain.style.backgroundImage="none";
+  headerBody.style.display = "none";
+  headerfooter.style.display = "none";
+  SwiperHeader.style.display = "flex";
+ }else{
+  headerMain.style.backgroundImage="url('../image/image 237.png'";
+ headerBody.style.display = "block";
+ headerfooter.style.display = "flex";
+ SwiperHeader.style.display = "none";
+ }
+
+
+}
 let int_num = 0;
 let num_span = 1;
+let cheackBoxI = 0;
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.header__ul-nav');
 menuBtn.addEventListener('click', function () {
@@ -43,13 +63,12 @@ function question_next() {
   let num = document.querySelector('.discount__body-question-count span');
 
   if (int_num < 9) {
-    console.log(window.innerWidth);
+
     if (window.innerWidth <= 1100) {
       bar.style.width = parseInt(bar.style.width, 10) + 36 + 'px';
     } else {
       bar.style.width = parseInt(bar.style.width, 10) + 66 + 'px';
     }
-
 
     num_span++;
     num.innerHTML = num_span;
@@ -137,10 +156,46 @@ function add_question(num) {
       elem.classList.add('discount__body-question-wrapper-item');
 
       elem.innerHTML = `
-        <input type="radio" />${arr[num][j]}
+        <input type="radio" value = 0 onclick = "unCheack(this)"/>${arr[num][j]}
       `;
 
       parent.appendChild(elem);
     }
   }
 }
+function unCheack(cheackBox){
+
+ if (cheackBox.value == 0 ){
+  cheackBox.checked = true;
+  cheackBox.value = 1
+ }else{
+  cheackBox.checked = false;
+  cheackBox.value = 0
+ }
+
+}
+
+document.querySelector('#phone').onkeydown = function(e){
+inputphone(e,document.querySelector('#phone'))
+}
+document.querySelector('#phone2').onkeydown = function(e){
+  inputphone(e,document.querySelector('#phone2'))
+  }
+
+
+
+
+function inputphone(e, phone){
+function stop(evt) {
+    evt.preventDefault();
+}
+let key = e.key, v = phone.value; not = key.replace(/([0-9])/, 1)
+
+if(not == 1 || 'Backspace' === not){
+if('Backspace' != not){ 
+    if(v.length < 3 || v ===''){phone.value= '+7('}
+    if(v.length === 6){phone.value= v +')'}
+    if(v.length === 10){phone.value= v +'-'}
+     if(v.length === 13){phone.value= v +'-'}
+    }
+}else{stop(e)}  }
